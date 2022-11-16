b00531024670f660caea00ffa8d78c048314af05
xiaofeicn <271986756@qq.com>
2022-11-16 20:42:15 +0800

    添加聊天

--- src/plugins/tools/chat.js
+++ src/plugins/tools/chat.js
@@ -5,7 +5,9 @@ async function chat(msg) {
     if (msg.raw_message.length < 1) {
         return msg.bot.say(msg.sid, "干嘛！", msg.type, msg.uid, true);
     }
-    const botCommands =global.config.botCommands.split(",")
+    const botCommands =['绑定','米游社','uid','深渊','上期深渊','我的','游戏内uid','信息','抽卡','卡池','定轨','查看定轨','取消定轨','圣遗物','强化',
+                       '副本','评分','周日','周一','周二','周三','周四','周五','周六','今日素材','周本 ','天赋 ','武器 ','点歌 ','音乐源','吃什么',
+                        '喝什么 ','求签 ','roll','带话','管理命令 ','help']
     var say=true;
     for (const botCommand in botCommands){
         if (msg.raw_message.toString().startsWith(botCommand)){
