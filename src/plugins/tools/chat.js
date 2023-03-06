e37522d87f04562825506036fb36249029fc7579
KleeScrawl <271986756@qq.com>
2023-03-06 15:08:26 +0800

    say

--- src/plugins/tools/chat.js
+++ src/plugins/tools/chat.js
@@ -17,28 +17,30 @@ async function chat(msg) {
     }
     if (!say) {
         return false;
-    }
-    const url =
-        "https://api.ownthink.com/bot?appid=f40e478ad5d244b3b286807ec5b46880" +
-        "&userid=" +
-        msg.uid +
-        "&spoken=" +
-        msg.raw_message;
-    try {
-        response = await fetch(url, {
-            method: "get",
-        });
-    } catch (e) {
+    }else {
+        const url =
+            "https://api.ownthink.com/bot?appid=f40e478ad5d244b3b286807ec5b46880" +
+            "&userid=" +
+            msg.uid +
+            "&spoken=" +
+            msg.raw_message;
+        try {
+            response = await fetch(url, {
+                method: "get",
+            });
+        } catch (e) {
+            msg.bot.say(msg.sid, "说什么呢！", msg.type, msg.uid, true);
+        }
+
+        if (200 === response.status) {
+            const { data } = await response.json();
+            const talk = data["info"]["text"].replaceAll("小思", "派蒙").replaceAll("思知", "提瓦特");
+            return msg.bot.say(msg.sid, `${talk}`, msg.type, msg.uid, true, "\n");
+        }
+
         msg.bot.say(msg.sid, "说什么呢！", msg.type, msg.uid, true);
     }
 
-    if (200 === response.status) {
-        const { data } = await response.json();
-        const talk = data["info"]["text"].replaceAll("小思", "派蒙").replaceAll("思知", "提瓦特");
-        return msg.bot.say(msg.sid, `${talk}`, msg.type, msg.uid, true, "\n");
-    }
-
-    msg.bot.say(msg.sid, "说什么呢！", msg.type, msg.uid, true);
 }
 
 export { chat };
